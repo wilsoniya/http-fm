bc9b2be7ec515c57a9f5cd8ef7c3f400f7d80873
Michael Wilson <wilsoniya@gmail.com>
2017-03-09 08:19:47 -0800

    removed unwrap() from views.rs

--- src/views.rs
+++ src/views.rs
@@ -1,10 +1,6 @@
 use std::collections::HashMap;
-use std::fs::DirEntry;
-use std::fs::File;
-use std::io::Result;
 use std::path::PathBuf;
 
-use rocket::response::Stream;
 use rocket_contrib::Template;
 
 use data::{DirItem, DirContext, CodeResponse};
@@ -23,29 +19,49 @@ pub fn share_dir(code: &str, path: PathBuf) -> Option<CodeResponse> {
         let abs_path = code_root.join(path);
 
         if abs_path.is_dir() {
-            let mut dir_items = abs_path.read_dir().unwrap()
-            .map(|dir_entry: Result<DirEntry>| {
-                let dir_entry = dir_entry.unwrap();
-                let path = dir_entry.path();
-                let rel_path = path.strip_prefix(&code_root).unwrap();
-                let item = rel_path.to_str().unwrap().to_owned();
-                let name = get_last_path_component(&rel_path.to_owned())
-                    .unwrap().to_owned();
-                DirItem { is_dir: path.is_dir(), path: item, name: name}
+            abs_path.read_dir()
+            .map(|dir_entries| {
+                let mut dir_items = dir_entries
+                .map(|maybe_dir_entry| {
+                    maybe_dir_entry.ok()
+                    .and_then(|dir_entry| {
+                        let path = dir_entry.path();
+                        path.strip_prefix(&code_root).ok()
+                        .and_then(|rel_path| {
+                            rel_path.to_str()
+                            .and_then(|item| {
+                                get_last_path_component(&rel_path.to_owned())
+                                .map(|name| {
+                                    let item = item.to_owned();
+                                    let name = name.to_owned();
+                                    DirItem { is_dir: path.is_dir(), path: item, name: name }
+                                })
+                            })
+                        })
+                    })
+                })
+                .filter_map(|item| item)
+                .filter(|dir_item| !is_hidden(&PathBuf::from(&dir_item.path)))
+                .collect::<Vec<_>>();
+
+                dir_items.sort();
+
+                dir_items
+            }).ok()
+            .and_then(|dir_items| {
+                abs_path.strip_prefix(&code_root).ok()
+                .and_then(|code_rel_path| {
+                    PathBuf::from(code).join(code_rel_path).to_str()
+                    .map(|dpath| {
+                        let dpath = dpath.to_owned();
+                        let code_str = code.to_owned();
+                        let context = DirContext {
+                            dpath: dpath, items: dir_items, code: code_str
+                        };
+                        CodeResponse::Directory(Template::render("dir", &context))
+                    })
+                })
             })
-            .filter(|di| !is_hidden(&PathBuf::from(&di.path)))
-            .collect::<Vec<DirItem>>();
-            dir_items.sort();
-
-            let dpath = PathBuf::from(code).join(
-                abs_path.strip_prefix(&code_root).unwrap())
-                .to_str().unwrap().to_owned();
-
-            let code_str = code.to_owned();
-            let context = DirContext {
-                dpath: dpath, items: dir_items, code:
-                    code_str.clone() };
-            Some(CodeResponse::Directory(Template::render("dir", &context)))
         } else {
             Some(CodeResponse::Blob(abs_path))
         }
@@ -75,8 +91,7 @@ pub fn share(code: &str) -> Option<CodeResponse> {
 
 #[get("/")]
 pub fn index() -> Template {
-    let context: HashMap<&str, &str> = HashMap::new();
-    Template::render("index", &context)
+    let context: HashMap<&str, &str> = HashMap::new(); Template::render("index", &context)
 }
 
 fn resolve_code_fpath(code: &str) -> Option<PathBuf> {
