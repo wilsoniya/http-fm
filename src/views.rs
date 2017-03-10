0d0981106d23a5f10baca21093a0cfaaf072cfa4
Michael Wilson <wilsoniya@gmail.com>
2017-03-09 21:14:10 -0800

    documentation and type annotation

--- src/views.rs
+++ src/views.rs
@@ -16,22 +16,29 @@ static FPATH2: &'static str = "/home/wilsoniya";
 pub fn share_dir(code: &str, path: PathBuf) -> Option<CodeResponse> {
     resolve_code_fpath(code)
     .and_then(|code_root| {
+        // case: code resolves to an actual path
         let abs_path = code_root.join(path);
 
         if abs_path.is_dir() {
             abs_path.read_dir()
             .map(|dir_entries| {
+                // case: success in reading stuff out of code_root dir
                 let mut dir_items = dir_entries
                 .map(|maybe_dir_entry| {
+                    // for each item in in code_root dir
                     maybe_dir_entry.ok()
                     .and_then(|dir_entry| {
+                        // case: item inside code_root is ok
                         let path = dir_entry.path();
                         path.strip_prefix(&code_root).ok()
                         .and_then(|rel_path| {
+                            // case: successfully stripped code_root prefix from item
                             rel_path.to_str()
                             .and_then(|item| {
+                                // case: rel_path successfully stringified
                                 get_last_path_component(&rel_path.to_owned())
                                 .map(|name| {
+                                    // case: success got last path component
                                     let item = item.to_owned();
                                     let name = name.to_owned();
                                     DirItem { is_dir: path.is_dir(), path: item, name: name }
@@ -40,19 +47,22 @@ pub fn share_dir(code: &str, path: PathBuf) -> Option<CodeResponse> {
                         })
                     })
                 })
-                .filter_map(|item| item)
-                .filter(|dir_item| !is_hidden(&PathBuf::from(&dir_item.path)))
-                .collect::<Vec<_>>();
+                .filter_map(|item: Option<DirItem>| item)
+                .filter(|dir_item: &DirItem| !is_hidden(&PathBuf::from(&dir_item.path)))
+                .collect::<Vec<DirItem>>();
 
                 dir_items.sort();
 
                 dir_items
             }).ok()
             .and_then(|dir_items| {
+                // case: DirItem Vec created successfully
                 abs_path.strip_prefix(&code_root).ok()
                 .and_then(|code_rel_path| {
+                    // case: code_root relative path derived successfully
                     PathBuf::from(code).join(code_rel_path).to_str()
                     .map(|dpath| {
+                        // case: code_root relative path sucessfully stringified
                         let dpath = dpath.to_owned();
                         let code_str = code.to_owned();
                         let context = DirContext {
