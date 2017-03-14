3ef080b31d1cb6d27af5e0132936f9ed74634167
Allen Nelson <anelson@narrativescience.com>
2017-03-14 16:49:26 -0500

    add PORT command-line argument, fix type error

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,15 @@
 #![plugin(rocket_codegen)]
 
 extern crate chrono;
+extern crate clap;
 extern crate rand;
 extern crate rocket;
 extern crate rocket_contrib;
 extern crate rusqlite;
 extern crate serde_json;
+
+use clap::{App, Arg};
+use rocket::config::{Config, Environment};
 
 pub mod data;
 pub mod db;
@@ -16,7 +20,20 @@ pub mod views;
 #[macro_use] extern crate serde_derive;
 
 fn main() {
-    rocket::ignite()
+    let matches = App::new("http-fm daemon")
+	.about("Serves files and directories")
+        .arg(Arg::with_name("PORT")
+             .help("http-fm server listening port")
+             .short("p")
+             .long("port")
+             .default_value("8000"))
+	.get_matches();
+    let config = Config::build(Environment::Staging)
+        .address("0.0.0.0")
+        .port(matches.value_of("PORT").unwrap().parse().unwrap())
+        .finalize().unwrap();
+
+    rocket::custom(config, true)
         .mount("/", routes![views::index, views::share_dir, views::share])
         .launch();
 }
